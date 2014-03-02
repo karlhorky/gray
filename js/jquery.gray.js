d9229d07f0f907b8da8e0ff5490341c19bf09a1c
Karl Horky <karl.horky@gmail.com>
2014-03-02 17:49:49 +0100

    IE shim: copy styles from grayscale element to replacement element. Fixes #1

--- js/jquery.gray.js
+++ js/jquery.gray.js
@@ -40,6 +40,21 @@
       return type;
     },
 
+    getComputedStyle: function(element) {
+      var computedStyle = {},
+          styles        = {};
+
+      computedStyle = window.getComputedStyle(element, null);
+
+      for(var i = 0, length = computedStyle.length; i < length; i++) {
+        var prop = computedStyle[i];
+        var val = computedStyle.getPropertyValue(prop);
+        styles[prop] = val;
+      }
+
+      return styles;
+    },
+
     extractUrl: function(backgroundImage) {
       var url,
           regex;
@@ -63,26 +78,6 @@
       margin = 'margin:' + y + ' 0 0 ' + x;
 
       return margin;
-    },
-
-    getParams: function(element) {
-      var type = this.elementType(element);
-      return this['get' + type + 'Params'](element);
-    },
-
-    getImgParams: function(element) {
-      var params = {};
-
-      params.width  = element.width();
-      params.height = element.height();
-      params.svg    = {
-        url   : element[0].src,
-        width : params.width,
-        height: params.height,
-        offset: ''
-      };
-
-      return params;
     },
 
     getBgSize: function(url, backgroundSize) {
@@ -126,6 +121,26 @@
       return size;
     },
 
+    getParams: function(element) {
+      var type = this.elementType(element);
+      return this['get' + type + 'Params'](element);
+    },
+
+    getImgParams: function(element) {
+      var params = {};
+
+      params.styles = this.getComputedStyle(element[0]);
+
+      params.svg = {
+        url   : element[0].src,
+        width : params.styles.width.replace('px', ''),
+        height: params.styles.height.replace('px', ''),
+        offset: ''
+      };
+
+      return params;
+    },
+
     getBgParams: function(element) {
       var params = {},
           url,
@@ -135,9 +150,9 @@
       bgSize    = this.getBgSize(url, element.css('background-size'))
       offset    = this.positionToNegativeMargin(element.css('background-position'));
 
-      params.width  = element.width();
-      params.height = element.height();
-      params.svg    = $.extend(
+      params.styles = this.getComputedStyle(element[0]);
+
+      params.svg = $.extend(
         { url   : url },
         bgSize,
         { offset: offset }
@@ -152,9 +167,9 @@
 
       params = this.getParams(element);
 
-      // TODO: use templating here
+      // TODO: use templating or DOM elements here
       template = $(
-        '<div style="display:inline-block;overflow:hidden;width:'+params.width+'px;height:'+params.height+'px;">' +
+        '<div>' +
           '<svg xmlns="http://www.w3.org/2000/svg" id="svgroot" viewBox="0 0 '+params.svg.width+' '+params.svg.height+'" width="'+params.svg.width+'" height="'+params.svg.height+'" style="'+params.svg.offset+'">' +
             '<defs>' +
               '<filter id="gray">' +
@@ -165,6 +180,14 @@
             '<image filter="url(&quot;#gray&quot;)" x="0" y="0" width="'+params.svg.width+'" height="'+params.svg.height+'" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="'+params.svg.url+'" />' +
           '</svg>' +
         '</div>');
+
+      params.styles['display']  = 'inline-block';
+      params.styles['overflow'] =
+        params.styles['overflow-x'] =
+        params.styles['overflow-y'] = 'hidden';
+
+      // TODO: Should this really set all params or should we set only unique ones by comparing to a control element?
+      template.css(params.styles);
 
       element.replaceWith(template);
     }
